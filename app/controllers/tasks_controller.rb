afc22bd5764530f19e6943371fcabbe14f8decf6
devmuturi <muturinjonge09@gmail.com>
2025-12-10 07:08:34 +0300

    Added ability to delete a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
-  before_action :load_task!, only: %i[show update]
+  before_action :load_task!, only: %i[show update destroy]
   def index
     tasks = Task.all
     render status: :ok, json: { tasks: }
@@ -20,6 +20,11 @@ class TasksController < ApplicationController
   def update
     @task.update!(task_params)
     render_notice(t("successfully_updated"))
+  end
+
+  def destroy
+    @task.destroy!
+    render_json
   end
 
   private
--- app/javascript/src/apis/tasks.js
+++ app/javascript/src/apis/tasks.js
@@ -11,6 +11,8 @@ const update = ({ slug, payload }) =>
     task: payload,
   });
 
-const tasksApi = { fetch, create, show, update };
+const destroy = slug => axios.delete(`/tasks/${slug}`);
+
+const tasksApi = { fetch, create, show, update, destroy };
 
 export default tasksApi;
