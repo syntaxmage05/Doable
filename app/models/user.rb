0d0dc11b9c2f37968d6db8b5c789587ec9999616
devmuturi <muturinjonge09@gmail.com>
2025-12-13 08:10:15 +0300

    Added task owner to tasks

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,16 @@ class User < ApplicationRecord
   MAX_EMAIL_LENGTH = 255
 
   has_many :assigned_tasks, foreign_key: :assigned_user_id, class_name: "Task"
-  has_secure_password
-  has_secure_token :authentication_token
-
+  has_many :created_tasks, foreign_key: :task_owner_id, class_name: "Task"
   validates :name, presence: true, length: { maximum: MAX_NAME_LENGTH }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: MAX_EMAIL_LENGTH },
     format: { with: VALID_EMAIL_REGEX }
   validates :password, length: { minimum: MIN_PASSWORD_LENGTH }, if: -> { password.present? }
   validates :password_confirmation, presence: true, on: :create
+  before_destroy :assign_tasks_to_task_owners
+
+  has_secure_password
+  has_secure_token :authentication_token
 
   before_save :to_lowercase
 
@@ -22,5 +24,12 @@ class User < ApplicationRecord
 
     def to_lowercase
       email.downcase!
+    end
+
+    def assign_tasks_to_task_owners
+      tasks_whose_owner_is_not_current_user = assigned_tasks.where.not(task_owner_id: id)
+      tasks_whose_owner_is_not_current_user.find_each do |task|
+        task.update(assigned_user_id: task.task_owner_id)
+      end
     end
 end
--- app/views/tasks/show.json.jbuilder
+++ app/views/tasks/show.json.jbuilder
@@ -9,4 +9,8 @@ json.task do
   json.assigned_user do
     json.extract! @task.assigned_user, :id, :name
   end
+
+  json.task_owner do
+    json.extract! @task.task_owner, :name
+  end
 end
